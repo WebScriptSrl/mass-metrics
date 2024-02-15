47f8adebe2263cade1bc634436e7997293091d7d
calyn05 <67522453+calyn05@users.noreply.github.com>
2024-02-15 15:17:12 +0200

    Remove unused function

--- lib/data/actions.ts
+++ lib/data/actions.ts
@@ -2,7 +2,6 @@
 
 import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
-import { getStatus } from ".";
 
 export async function revalidateData(tag: string) {
   revalidateTag(tag);
@@ -17,15 +16,3 @@ export async function setCookie(name: string, value: string) {
     cookie: cookieStore.set(name, value),
   };
 }
-
-export const connectionsStats = async () => {
-  const response = getStatus().then((data) => {
-    for (const [key, value] of Object.entries(data.result.connected_nodes)) {
-      // console.log(`${key}: ${value[0]} ${value[1]}`);
-
-      if (value[1] === false) {
-        console.log(`Node ${key} is in connection`);
-      }
-    }
-  });
-};
